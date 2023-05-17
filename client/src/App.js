779ae3de802e34ce0224a51bfa6d0dae826074d6
Kim-DaHam <81691456+Kim-DaHam@users.noreply.github.com>
2023-05-17 12:11:36 +0900

    design/add App.css & import in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import './App.css';
+
 import MainPage from './pages/MainPage/MainPage'
 import Header from './components/Header/Header'
 
