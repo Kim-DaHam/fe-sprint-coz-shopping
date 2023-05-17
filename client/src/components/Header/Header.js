2bf97899885907abe2613ae8ed6a1ec8650778de
Kim-DaHam <81691456+Kim-DaHam@users.noreply.github.com>
2023-05-17 14:40:50 +0900

    feat/Header 로고&메뉴 기본 틀 완료

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,10 +1,52 @@
 import React from 'react'
+import styled from "styled-components"
+
+const HeaderContainer = styled.div`
+        width: 100vw;
+        height: 50px;
+        padding: 0 60px;
+        position: fixed;
+        justify-content: space-between;
+        align-items: center;
+        border: 1px red solid;
+        display: flex;
+    `;
+
+const LogoImageWrapper = styled.div`
+    float: left;
+    display: flex;
+    gap: 10px;
+    &:hover {
+        cursor: pointer;
+    }
+`;
+
+const LogoImg = styled.img`
+    height: 25px;
+`;
+
+const LogoTitle = styled.div`
+    font-weight: 600;
+    font-size: 1.2rem;
+`;
+
+const MenuIcon = styled.img`
+    height: 18px;
+    &:hover {
+        cursor: pointer;
+    }
+`;
 
 function Header(){
+
     return (
-        <div className='header'>
-            헤더 입니다.
-        </div>
+        <HeaderContainer>
+            <LogoImageWrapper>
+                <LogoImg src="./images/logo/로고.png"/>
+                <LogoTitle>COZ Shopping</LogoTitle>
+            </LogoImageWrapper>
+            <MenuIcon src="./images/icons/hamburger.png"></MenuIcon>
+        </HeaderContainer>
     )
 }
 
